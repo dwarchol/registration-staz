636b8bee5b736af1155dcf20f6283e2f6b24bcec
DW <dosiwa@wp.pl>
2020-03-30 10:00:35 +0200

    Updates

---
+++ precache-manifest.32e14dcffd525b6826dd647a1f59e06b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db2651965403e67856cc1c3d2ea81357",
+    "url": "/registration/index.html"
+  },
+  {
+    "revision": "978a1a3df8b70d6e3ae8",
+    "url": "/registration/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b80e6497d5fc3b37c5f1",
+    "url": "/registration/static/js/2.a6ea239f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/registration/static/js/2.a6ea239f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "978a1a3df8b70d6e3ae8",
+    "url": "/registration/static/js/main.d6a9561c.chunk.js"
+  },
+  {
+    "revision": "a84f8df832a6e92cce8a",
+    "url": "/registration/static/js/runtime-main.86956daf.js"
+  }
+]);
